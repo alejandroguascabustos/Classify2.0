c09f3e7d11085326fdac114e092d7aff06c73e22
Alejandroguascabustos <alejandro-guasca@outlook.com>
2026-03-21 09:05:21 -0500

    inclusión de controladores return mostar

--- src/main/java/com/classify20/controller/VistasController.java
+++ src/main/java/com/classify20/controller/VistasController.java
@@ -7,11 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class VistasController {
     
 
-    @GetMapping("/inicio")
-    public String mostrarHome(){
-        return "inicio/Inicio";
+    @GetMapping("/agenda")
+    public String mostrarAgenda(){
+        return "agenda/agenda";
     }
-
+    @GetMapping("/aprende")
+    public String mostrarAprende(){
+        return "aprende/aprende";
+    }
     @GetMapping("/login")
     public String mostrarLogin(){
         return "auth/login";
@@ -20,14 +23,69 @@ public class VistasController {
     public String mostrarRegisto(){
         return "auth/registro";
     }
+    @GetMapping("/califica")
+    public String mostrarCalifica(){
+        return "califica/califica";
+    }
+    @GetMapping("/conoce")
+    public String mostrarConoce(){
+        return "conoce/conoce";
+    }
+    @GetMapping("/contacta")
+    public String mostrarContacta(){
+        return "contacta/contacta";
+    }
+    @GetMapping("/contacto")
+    public String mostrarContacto(){
+        return "contacto/contacto";
+    }
+    @GetMapping("/inicio")
+    public String mostrarInicio(){
+        return "inicio/inicio";
+    }
+    @GetMapping("/izada")
+    public String mostrarIzada(){
+        return "izada/izada";
+    }
+    @GetMapping("/materiales")
+    public String mostrarMateriales(){
+        return "materiales/materiales";
+    }
     @GetMapping("/menu")
     public String mostrarMenu(){
         return "menu/menu";
     }
-
-    @GetMapping("/contacto")
-    public String mostrarContacto(){
-        return "contacto/contacto";
+    @GetMapping("/materiales")
+    public String mostrarMismateriales(){
+        return "mismateriales/mismateriales";
+    }
+    @GetMapping("/nosotros")
+    public String mostrarNosotros(){
+        return "nosotros/nosotros";
+    }
+    @GetMapping("/notas")
+    public String mostrarNotas(){
+        return "notas/notas";
+    }
+    @GetMapping("/noticias")
+    public String mostrarNoticias(){
+        return "noticias/noticias";
+    }
+    @GetMapping("/politicas")
+    public String mostrarPoliticas(){
+        return "politicas/politicas";
+    }
+    @GetMapping("/programacion")
+    public String mostrarProgramacion(){
+        return "programacion/programacion";
+    }
+    @GetMapping("/soporte")
+    public String mostrarSoporte(){
+        return "soporte/soporte";
+    }
+    @GetMapping("/terminos")
+    public String mostrarTerminos(){
+        return "terminos/terminos";
     }
 
     @GetMapping("/")
