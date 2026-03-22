d83e3e2b15202831d68253cfb318d5e8cc991c15
Alejandroguascabustos <alejandro-guasca@outlook.com>
2026-03-22 07:39:47 -0500

    vista contacto.html

--- src/main/resources/static/js/prealoader.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', () => {
-  const preloader = document.getElementById('preloader');
-  if (preloader) {
-      preloader.style.opacity = '0';
-      setTimeout(() => { preloader.style.display = 'none'; }, 1000);
-  }
-});
