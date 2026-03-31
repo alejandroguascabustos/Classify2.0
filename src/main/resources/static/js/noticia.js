1289a3867e6a55b178118346d85f6418841e56d7
Jhon Jairo Lopez Quiroga <quirogajon90@gmail.com>
2026-03-31 14:23:22 -0500

    se separa el js de formulario y crea un nuevo archivo noticias.js en la carpeta js y se modifica un poco la vista del menu

---
+++ src/main/resources/static/js/noticia.js
@@ -0,0 +1,14 @@
+// Muestra preview de la imagen antes de guardar
+        function previewNuevaImagen(input) {
+            const preview = document.getElementById('nuevaPreview');
+            if (input.files && input.files[0]) {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    preview.src = e.target.result;
+                    preview.style.display = 'block';
+                };
+                reader.readAsDataURL(input.files[0]);
+            } else {
+                preview.style.display = 'none';
+            }
+        }
